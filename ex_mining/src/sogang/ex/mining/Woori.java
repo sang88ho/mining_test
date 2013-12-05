6e45a82353bae4c4a0e48b656b07a7dd1e940c66
WangSang <sang88ho@gmail.com>
2013-12-05 19:51:47 +0900

    Mining Test Base

---
+++ ex_mining/src/sogang/ex/mining/Woori.java
@@ -0,0 +1,103 @@
+package sogang.ex.mining;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import sogang.ex.mining.exchange.Exchange;
+
+public class Woori {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		Document doc = Jsoup.connect("https://spot.wooribank.com/pot/jcc?withyou=FXXRT0011&__ID=c008329").get();
+		Elements ddElements = doc.getElementsByTag("dd");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+		Date date = sdf.parse(ddElements.get(0).text());
+		
+		Elements contents = doc.getElementsByTag("td");
+
+		int i = 0;
+		boolean find = false;
+
+		List<Exchange> exhanges = new ArrayList<Exchange>();
+		Exchange ex = null;
+		for (Element content : contents) {
+			String text = content.text();
+			if (text.equals("USD")) {
+				i = 0;
+				find = true;
+				ex = new Exchange("woori", "미국", "USD", date);
+				exhanges.add(ex);
+			} else if (text.equals("JPY")) {
+				i = 0;
+				find = true;
+				ex = new Exchange("woori", "일본", "JPY", date);
+				exhanges.add(ex);
+			} else if (text.equals("EUR")) {
+				i = 0;
+				find = true;
+				ex = new Exchange("woori", "유럽연합", "EUR", date);
+				exhanges.add(ex);
+			} else if (text.equals("CNY")) {
+				i = 0;
+				find = true;
+				ex = new Exchange("woori", "중국", "CNY", date);
+				exhanges.add(ex);
+			}
+			
+			if (find) {
+				if(text.contains(",")) {
+					StringTokenizer st = new StringTokenizer(text, ",");
+					
+					String removedText = "";
+					while (st.hasMoreElements()) { 
+						removedText += st.nextToken();
+					}
+					
+					text = removedText;
+				}
+
+				switch (i) {
+				case 2:
+					ex.setTransferSend(Float.parseFloat(text));
+					break;
+
+				case 3:
+					ex.setTransferReceive(Float.parseFloat(text));
+					break;
+					
+				case 4:
+					ex.setCashReceive(Float.parseFloat(text));
+					break;
+					
+				case 5:
+					ex.setCashSend(Float.parseFloat(text));					
+					break;
+					
+				case 7:
+					ex.setSaleStandard(Float.parseFloat(text));
+					break;
+					
+				case 9:
+					find = false;
+					ex.setUsdExchangeRate(Float.parseFloat(text));
+					break;
+					
+				default:
+					break;
+				}
+				++i;
+			}
+		}
+	}
+}
---
+++ ex_mining/src/sogang/ex/mining/exchange/Exchange.java
@@ -0,0 +1,139 @@
+package sogang.ex.mining.exchange;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Exchange {
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy. MM. dd HH:mm:ss");
+	
+	private Integer id;
+
+	private String bank;
+	
+	private String nation;
+
+	private String monetaryCode;
+	
+	private Float transferSend;
+	
+	private Float transferReceive;
+	
+	private Float cashSend;
+	
+	private Float cashReceive;
+	
+	private Float saleStandard;
+	
+	private Float usdExchangeRate;
+	
+	private Calendar date;
+
+	public Exchange() {
+		super();
+	}
+	
+	public Exchange(String bank, String nation, String monetaryCode, Date date) {
+		super();
+		this.bank = bank;
+		this.nation = nation;
+		this.monetaryCode = monetaryCode;
+		this.date = Calendar.getInstance();
+		this.date.setTime(date);
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public String getNation() {
+		return nation;
+	}
+
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+
+	public String getMonetaryCode() {
+		return monetaryCode;
+	}
+
+	public void setMonetaryCode(String monetaryCode) {
+		this.monetaryCode = monetaryCode;
+	}
+
+	public Float getTransferSend() {
+		return transferSend;
+	}
+
+	public void setTransferSend(Float transferSend) {
+		this.transferSend = transferSend;
+	}
+	
+	public Float getTransferReceive() {
+		return transferReceive;
+	}
+
+	public void setTransferReceive(Float transferReceive) {
+		this.transferReceive = transferReceive;
+	}
+
+	public Float getCashSend() {
+		return cashSend;
+	}
+
+	public void setCashSend(Float cashSend) {
+		this.cashSend = cashSend;
+	}
+
+	public Float getCashReceive() {
+		return cashReceive;
+	}
+
+	public void setCashReceive(Float cashReceive) {
+		this.cashReceive = cashReceive;
+	}
+
+	public Float getSaleStandard() {
+		return saleStandard;
+	}
+
+	public void setSaleStandard(Float saleStandard) {
+		this.saleStandard = saleStandard;
+	}
+
+	public Float getUsdExchangeRate() {
+		return usdExchangeRate;
+	}
+
+	public void setUsdExchangeRate(Float usdExchangeRate) {
+		this.usdExchangeRate = usdExchangeRate;
+	}
+
+	public String getDate() {
+		return date == null ? null : sdf.format(date.getTime());
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Exchange [id=" + id + ", bank=" + bank + ", nation=" + nation + ", monetaryCode=" + monetaryCode + ", transferSend=" + transferSend
+				+ ", transferReceive=" + transferReceive + ", cashSend=" + cashSend + ", cashReceive=" + cashReceive + ", saleStandard="
+				+ saleStandard + ", usdExchangeRate=" + usdExchangeRate + ", date=" + date + "]";
+	}
+}
